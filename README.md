6d167df607f16112ceb4b17e5f8c73690b09e386
Luis Alvarez <alvarez.en.luis@gmail.com>
2023-08-03 12:36:19 -0700

    Initial Project Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Angular_WorldCities
---
+++ WorldCitiesAPI/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using WorldCitiesAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var strConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+Console.WriteLine("This is the connection string from .json " + strConnection);
+
+builder.Services
+    .AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(strConnection));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
