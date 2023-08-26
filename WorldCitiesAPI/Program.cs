e7317e5b40b6f1d7416421ef0530997299d9576a
luis <alvarez.en.luis@gmail.com>
2023-08-26 10:24:13 -0700

    configured serilog and ready to webapi

--- WorldCitiesAPI/Program.cs
+++ WorldCitiesAPI/Program.cs
@@ -1,10 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using WorldCitiesAPI.Data;
 using Microsoft.AspNetCore.Cors;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Adds Serilog Support
+builder.Host.UseSerilog((ctx, lc) =>
+  lc.ReadFrom.Configuration(ctx.Configuration)
+  .WriteTo.MSSqlServer(connectionString: ctx.Configuration.GetConnectionString("DefaultConnection"),
+                        restrictedToMinimumLevel: LogEventLevel.Information,
+                        sinkOptions: new MSSqlServerSinkOptions { TableName = "LogEvents", AutoCreateSqlTable = true })
+  .WriteTo.Console()
+);
+
 
 builder.Services
   .AddControllers()
@@ -34,6 +47,8 @@ builder.Services
     .AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(strConnection));
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
