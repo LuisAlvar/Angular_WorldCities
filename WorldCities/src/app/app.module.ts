57e60d1ca09a0128769a2c52286a677088deae8b
luis <alvarez.en.luis@gmail.com>
2023-09-01 23:01:25 -0700

    done adding angular/pwa to front-end

--- WorldCities/src/app/app.module.ts
+++ WorldCities/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { CityService } from './cities/city.service';
 import { LoginComponent } from './auth/login.component';
 
 import { AuthInterceptor } from './auth/auth.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,12 @@ import { AuthInterceptor } from './auth/auth.interceptor';
   ],
   imports: [
     BrowserModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule,
-    AngularMaterialModule, ReactiveFormsModule
+    AngularMaterialModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})
   ],
   providers: [
     CityService,
